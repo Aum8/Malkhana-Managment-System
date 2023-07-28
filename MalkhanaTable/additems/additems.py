68437ae4d30e20b36a651aa241f73b8fa9c25a92
Aum8 <aum832003@gmail.com>
2023-07-28 16:15:47 +0530

    Added insert function to add items

--- MalkhanaTable/additems/additems.py
+++ MalkhanaTable/additems/additems.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import home.Homepage as Homepage
 import MalkhanaTable.MalkhanaPage as m
+import sqlite3
 
 def additems():
     global root
@@ -39,12 +40,44 @@ def additems():
     crime_inspector_entry.grid(row=7, column=1, padx=10, pady=10)
 
     home_button = tk.Button(root, text="Home", command=go_home)
-    home_button.grid(row=8, column=0, columnspan=2, padx=10, pady=10)
+    home_button.place(x=root.winfo_screenwidth() - 100, y=10, width=80, height=30)
+
 
     back_button = tk.Button(root, text="Back", command=go_back)
-    back_button.grid(row=8, column=2, columnspan=2, padx=10, pady=10)
+    back_button.place(x=root.winfo_screenwidth() - 200, y=10, width=80, height=30)
+
+    add_item_button = tk.Button(root, text="Add Item", command=insert_data)
+    add_item_button.grid(row=8, column=0, columnspan=4, padx=10, pady=10)
 
     root.mainloop()
+
+def insert_data():
+    # Retrieve data from the entry fields
+    global barcode_entry, item_name_entry, ipc_section_entry, crime_scene_entry, crime_date_entry, crime_time_entry, crime_witnesses_entry, crime_inspector_entry
+    barcode = barcode_entry.get()
+    item_name = item_name_entry.get()
+    ipc_section = ipc_section_entry.get()
+    crime_scene = crime_scene_entry.get()
+    crime_date = crime_date_entry.get()
+    crime_time = crime_time_entry.get()
+    crime_witnesses = crime_witnesses_entry.get()
+    crime_inspector = crime_inspector_entry.get()
+    
+    # Connect to the database (or create if it doesn't exist)
+    conn = sqlite3.connect('Items.db')
+    cursor = conn.cursor()
+    
+    # Execute the SQL command to insert data into the table
+    cursor.execute('''INSERT INTO items (barcode, item_name, ipc_section, 
+                      crime_scene, crime_date, crime_time, crime_witnesses, 
+                      crime_inspector) VALUES (?, ?, ?, ?, ?, ?, ?, ?)''',
+                   (barcode, item_name, ipc_section, crime_scene, crime_date, 
+                    crime_time, crime_witnesses, crime_inspector))
+    
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+
 
 def go_back():
     root.withdraw()
